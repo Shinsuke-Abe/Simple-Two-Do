6d5408d19234c80e8ed565be4ceeb6a07f73e1e8
Shinsuke Abe <mao.instantlife@gmail.com>
2012-01-29 00:03:37 +0900

    deleted build.scala

--- project/build.scala
+++
@@ -1,6 +0,0 @@
-import sbt._
-object MyApp extends Build
-{
-	lazy val root = Project("root", file(".")) dependsOn(unfilteredScalate)
-	lazy val unfilteredScalate = uri("http://github.com/unfiltered/unfiltered-scalate")
-}
