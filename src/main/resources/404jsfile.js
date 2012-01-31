c156599715bf4dd2f2e43ca3eca80cc5c08f4937
Shinsuke Abe <mao.instantlife@gmail.com>
2012-02-01 01:22:46 +0900

    static contents test add not under context file

---
+++ src/main/resources/404jsfile.js
@@ -0,0 +1,3 @@
+function notcontext() {
+    window.alert("this js file is not under context!");
+}
--- src/main/scala/Web.scala
+++ src/main/scala/Web.scala
@@ -5,7 +5,6 @@ import unfiltered.response._
 import util.Properties
 import unfiltered.scalate._
 
-// TODO 静的コンテンツ(CSS,js)
 // TODO 静的ページ
 // TODO session管理
 // TODO Twitter API
@@ -13,8 +12,9 @@ class App extends unfiltered.filter.Plan {
 
   def intent = {
     // Scalate Sample
-    case req @ GET(Path(Seg("scalate" :: Nil))) => Ok ~> Scalate(req, "hello.ssp")
-    case req @ GET(Path(Seg("scalatejs" :: Nil))) => Ok ~> Scalate(req, "hellojs.ssp")
+    case req@GET(Path(Seg("scalate" :: Nil))) => Ok ~> Scalate(req, "hello.ssp")
+    // static js sample
+    case req@GET(Path(Seg("scalatejs" :: Nil))) => Ok ~> Scalate(req, "hellojs.ssp")
     case GET(_) => Ok ~> ResponseString("Unfiltered on Heroku!")
   }
 }
@@ -22,7 +22,8 @@ class App extends unfiltered.filter.Plan {
 object Web {
   def main(args: Array[String]) {
     val port = Properties.envOrElse("PORT", "8080").toInt
-    unfiltered.jetty.Http(port).context("/public"){
+    unfiltered.jetty.Http(port).context("/public") {
+      // add context for static contents "src/main/resource/public"
       _.resources(getClass().getResource("/public/"))
     }.filter(new App).run
   }
