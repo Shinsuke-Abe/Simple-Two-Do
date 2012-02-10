f31d7b75780d542cfcc250631dc33c483322c104
Shinsuke Abe <mao.instantlife@gmail.com>
2012-02-10 22:27:14 +0900

    use test twitter api

--- src/main/scala/Web.scala
+++ src/main/scala/Web.scala
@@ -4,6 +4,7 @@ import unfiltered.request._
 import unfiltered.response._
 import util.Properties
 import unfiltered.scalate._
+import com.maosandbox.twitterutil.TwitterUtil._
 
 // TODO session管理
 // TODO POSTメソッドでbody部から値を取り出す
@@ -15,6 +16,23 @@ class App extends unfiltered.filter.Plan {
     case req@GET(Path(Seg("scalate" :: Nil))) => Ok ~> Scalate(req, "hello.ssp")
     // static js sample
     case req@GET(Path(Seg("scalatejs" :: Nil))) => Ok ~> Scalate(req, "hellojs.ssp")
+    // get my timeline
+    case req@GET(Path(Seg("lasttweet" :: Nil))) => {
+      // マルチバイト対応(というかブラウザのデフォルトエンコーディングによる文字化けの対応)のため、
+      // HtmlContentを明示的に指定する
+      Ok ~> HtmlContent ~> ResponseString(getUserLastTweet("mao_instantlife"))
+    }
+    case req@GET(Path(Seg("gettimeline" :: Nil))) => {
+      val statuses = getUserTimeLine("mao_instantlife")
+
+      var tweetStr = new StringBuilder
+      statuses.foreach(tweet => tweetStr.append(tweet.getText).append("<br/>"))
+
+      Ok ~> HtmlContent ~> ResponseString(tweetStr.toString())
+    }
+    // TODO 自分で自分に出したつぶやきの取得
+    // TODO OAuth
+    // TODO タスクの状態の永続化(Mongo-DBを使う予定)
     // sessionテスト
     // login -> session(cookie)にユーザ情報追加
     case GET(_) => Ok ~> ResponseString("Unfiltered on Heroku!")
---
+++ src/main/scala/com/maosandbox/twitterutil/TwitterUtil.scala
@@ -0,0 +1,25 @@
+package com.maosandbox.twitterutil
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: mao
+ * Date: 12/02/10
+ * Time: 21:45
+ * To change this template use File | Settings | File Templates.
+ */
+
+import twitter4j._
+import scala.collection.JavaConverters._
+import scala.collection.mutable._
+
+object TwitterUtil {
+  val twitter = new TwitterFactory().getInstance()
+
+  def getUserTimeLine(user: String): Buffer[Status] = {
+    asScalaBufferConverter(twitter.getUserTimeline(user)).asScala
+  }
+
+  def getUserLastTweet(user: String): String = {
+    getUserTimeLine(user).head.getText
+  }
+}
